5834339108bf2624b14482223ead573c08216496
Hernan <hjviltez@gmail.com>
2024-05-02 16:17:40 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import com.google.gson.GsonBuilder;
 import models.Titulo;
 import models.TituloOmdb;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -47,5 +50,21 @@ public class Main {
                 System.out.println(e.getMessage());
             }*/
 
+            /*crear archivos de escritura*/
+            File file = new File("C:\\miArchivo.txt");
+            FileReader reader = new FileReader(file);
+
+            int data = reader.read();
+            while (data != -1) {
+                System.out.print((char) data);
+                data = reader.read();
+            }
+            reader.close();}
+
+            /*escribir en un archivo de escritura*/
+            File file = new File("C:\\salida.txt");
+            FileWriter writer = new FileWriter(file);
+            writer.write("Hola mundo!");
+            writer.close();
+
     }
-}
